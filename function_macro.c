fa8f621d30c6c1bcb9be936953748835c96b711f
Vishniew <vishnukesari14@gmail.com>
2024-12-30 18:19:04 +0530

    Initial commit on an empty branch

--- function_macro.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#define ADD(a, b) a + b
-int main()
-{
-	int x=5,y=6;
-	int sum = ADD(x,y);
-	printf("SUM: %d\n",sum);
-}
--- variable_pointer.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int *ptr = NULL;
-	int x =13;
-	ptr = &x;
-	printf("Accessed value through pointer: %d\n",*ptr);
-}
